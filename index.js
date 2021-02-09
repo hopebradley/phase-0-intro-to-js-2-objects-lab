abab5d4496c9fed217453ecffba0babf1b830f3a
Hope Bradley <hope.e.bradley@gmail.com>
2021-02-09 11:10:10 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+
+const employee = {name: "Hope", streetAddress: "2417 Bradcliff Dr NE"};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+	return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+	obj[key] = value;
+	return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+	const ans = Object.assign({}, obj);
+	delete ans.[key];
+	return ans;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+	delete obj.[key];
+	return obj;
+}
